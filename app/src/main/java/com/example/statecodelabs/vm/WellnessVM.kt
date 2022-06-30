a120da06c31456acdac7abdc839740b93d187579
AndreaP <andreapartenope@gmail.com>
2022-06-30 02:02:00 +0200

    end commit

---
+++ app/src/main/java/com/example/statecodelabs/vm/WellnessVM.kt
@@ -0,0 +1,24 @@
+package com.example.statecodelabs.vm
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+import com.example.statecodelabs.data.WellnessTask
+
+class WellnessVM : ViewModel() {
+    private val _tasks = getWellnessTasks().toMutableStateList()
+
+    val tasks: List<WellnessTask>
+        get() = _tasks
+
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
+    }
+
+    fun changeTaskChecked(item: WellnessTask, checked: Boolean) =
+        tasks.find { it.id == item.id }?.let { task ->
+            task.checked = checked
+        }
+
+    private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+}
+
